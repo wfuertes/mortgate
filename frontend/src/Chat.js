73f5d53d20bc0377a9e3f47b0e016b3277767f70
Willian F. Batista <wfuertes@gmail.com>
2016-10-22 19:04:59 -0200

    Adding chat page and home form

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import './Chat.css';
 
 export default class ChatBox extends React.Component {
     render() {
         return (
             <div className="content">
                 <h2 className="content-head is-center">MORTGATE super chat</h2>
-                chat whith the server bot
+                <div className="chat-content">
+                    <div className="bot-profile">
+                        I'm the ChatBot!
+                    </div>
+                    <div className="message-history">
+                        <ul>
+                            <li className="you">2016/10/01 16:44 (you): bla bla bla bla bla</li>
+                            <li className="bot">2016/10/01 16:45 (bot): blw ble ble ble bla</li>
+                            <li className="bot">2016/10/01 16:46 (bot): bla bla bla bla bla</li>
+                            <li className="you">2016/10/01 16:47 (you): bla bla bla bla bla</li>
+                            <li className="you">2016/10/01 16:44 (you): bla bla bla bla bla</li>
+                            <li className="bot">2016/10/01 16:45 (bot): blw ble ble ble bla</li>
+                            <li className="bot">2016/10/01 16:46 (bot): bla bla bla bla bla</li>
+                            <li className="you">2016/10/01 16:47 (you): bla bla bla bla bla</li>
+                            <li className="you">2016/10/01 16:44 (you): bla bla bla bla bla</li>
+                            <li className="bot">2016/10/01 16:45 (bot): blw ble ble ble bla</li>
+                            <li className="bot">2016/10/01 16:46 (bot): bla bla bla bla bla</li>
+                            <li className="you">2016/10/01 16:47 (you): bla bla bla bla bla</li>
+                            <li className="you">2016/10/01 16:44 (you): bla bla bla bla bla</li>
+                            <li className="bot">2016/10/01 16:45 (bot): blw ble ble ble bla</li>
+                            <li className="bot">2016/10/01 16:46 (bot): bla bla bla bla bla</li>
+                            <li className="you">2016/10/01 16:47 (you): bla bla bla bla bla</li>
+                            <li className="you">2016/10/01 16:44 (you): bla bla bla bla bla</li>
+                            <li className="bot">2016/10/01 16:45 (bot): blw ble ble ble bla</li>
+                            <li className="bot">2016/10/01 16:46 (bot): bla bla bla bla bla</li>
+                            <li className="you">2016/10/01 16:47 (you): bla bla bla bla bla</li>
+                            <li className="you">2016/10/01 16:44 (you): bla bla bla bla bla</li>
+                            <li className="bot">2016/10/01 16:45 (bot): blw ble ble ble bla</li>
+                        </ul>
+                    </div>
+                    <div className="message-input">
+                        <textarea />
+                    </div>
+                </div>
             </div>
         );
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { browserHistory, Router, Route, IndexRoute } from 'react-router'
+
 import App from './App';
 import HomeBox from './Home';
 import AboutBox from './About';
 import ChatBox from './Chat';
 import './index.css';
-import { Router, Route, browerHistory, IndexRoute } from 'react-router';
 
 ReactDOM.render(
-  (<Router history={browerHistory}>
+  (<Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute path="/" component={HomeBox} />
+      <IndexRoute component={HomeBox} />
       <Route path="/chat" component={ChatBox} />
       <Route path="/about" component={AboutBox} />
     </Route>
