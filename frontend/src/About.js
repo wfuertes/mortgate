ac50a4ae14c3fcd5ab654b5fe18ad37177472939
Willian F. Batista <wfuertes@gmail.com>
2016-10-22 15:49:13 -0200

    Frontend base struct and layout

---
+++ frontend/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class AboutBox extends React.Component {
+    render() {
+        return (
+            <div className="content">
+                <h2 className="content-head is-center">MORTGATE About</h2>
+                MORTGATE About
+            </div>
+        );
+    }
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './css/pure-min.css';
+import './css/marketing.css';
+import './css/grids-responsive-min.css';
+import { Link } from 'react-router';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div className="header">
+            <div className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+                <Link className="pure-menu-heading" to="/">MORTGATE</Link>
+
+                <ul className="pure-menu-list">
+                    <li className="pure-menu-item pure-menu-selected"><Link to="/" className="pure-menu-link">Home</Link></li>
+                    <li className="pure-menu-item"><Link to="/about" className="pure-menu-link">About</Link></li>
+                </ul>
+            </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="content-wrapper">
+            <div className="content">
+                {this.props.children}
+            </div>
+
+            <div className="footer l-box is-center">
+                MORTGATE: Made with love by the MapleStack Team.
+            </div>
+        </div>
       </div>
     );
   }
