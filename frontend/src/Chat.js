fd6ed7388746f51e34b5917f660236cdf507f335
Willian F. Batista <wfuertes@gmail.com>
2016-10-23 13:02:52 -0200

    chat with socket trying

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import io from 'socket.io-client';
 import './Chat.css';
 
+let socket = io("/");
+
 export default class ChatBox extends React.Component {
+    constructor() {
+        super();
+        this._key = 0;
+        this.nextKey = this.nextKey.bind(this);
+        this.state = { history: [], meMsg : ''};
+        this.setMeMsg = this.setMeMsg.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
+    }
+
+    nextKey() {
+        this._key += 1;
+        return this._key;
+    }
+
+    componentDidMount() {
+        socket.on('server-to-client-message', msg => {
+            console.log('______________Receiving message from socket');
+            console.log(msg);
+            this.setState({
+                history: [].concat(this.state.history, { author: 'bot', content: msg, id: this.nextKey() })
+            });
+
+            console.log(this.state);
+        });
+    }
+
+    setMeMsg(event) {
+        this.setState({ meMsg: event.target.value });
+    }
+
+    sendMessage(event) {
+        event.preventDefault();
+        console.log('______________Sending message to server!' + this.state.meMsg);
+        socket.emit('client-to-server-message', this.state.meMsg);
+        this.setState({
+            history: [].concat(this.state.history, { author: 'me', content: this.state.meMsg, id: this.nextKey() }),
+            meMsg: ''
+        });
+    }
+
     render() {
         return (
             <div className="content">
@@ -12,32 +55,18 @@ export default class ChatBox extends React.Component {
                     </div>
                     <div className="message-history">
                         <ul>
-                            <li className="me">me: bla bla bla bla bla</li>
-                            <li className="bot">blw ble ble ble bla :bot</li>
-                            <li className="bot">bla bla bla bla bla :bot</li>
-                            <li className="me">me: bla bla bla bla bla</li>
-                            <li className="me">me: bla bla bla bla bla</li>
-                            <li className="bot">blw ble ble ble bla :bot</li>
-                            <li className="bot">bla bla bla bla bla :bot</li>
-                            <li className="me">me: bla bla bla bla bla</li>
-                            <li className="me">me: bla bla bla bla bla</li>
-                            <li className="bot">blw ble ble ble bla :bot</li>
-                            <li className="bot">bla bla bla bla bla :bot</li>
-                            <li className="me">me: bla bla bla bla bla</li>
-                            <li className="me">me: bla bla bla bla bla</li>
-                            <li className="bot">blw ble ble ble bla :bot</li>
-                            <li className="bot">bla bla bla bla bla :bot</li>
-                            <li className="me">me: bla bla bla bla bla</li>
-                            <li className="me">me: bla bla bla bla bla</li>
-                            <li className="bot">blw ble ble ble bla :bot</li>
-                            <li className="bot">bla bla bla bla bla :bot</li>
-                            <li className="me">me: bla bla bla bla bla :bot</li>
-                            <li className="me">me: bla bla bla bla bla :bot</li>
-                            <li className="bot">blw ble ble ble bla :bot</li>
+                            {
+                                this.state.history.map(function (m) {
+                                    return (<li key={m.id} className={m.author}>{m.author}: {m.content}</li>);
+                                })
+                            }
                         </ul>
                     </div>
                     <div className="message-input">
-                        <input type="text" id="name" value="" placeholder="Type your message here" />
+                        <form method="post" onSubmit={this.sendMessage}>
+                            <input type="text" id="name" value={this.state.meMsg} onChange={this.setMeMsg} placeholder="Type your message here" />
+                            <button type="submit">Send</button>
+                        </form>
                     </div>
                 </div>
             </div>
